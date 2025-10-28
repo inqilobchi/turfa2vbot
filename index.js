19e9a65252e0ec83d880c738aa95d14f7fcb99ca
inqilobchi <hieojody@gmail.com>
2025-10-28 12:52:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -294,7 +294,7 @@ async function scrapeSite(url) {
       }
     }
     console.log(`✅ Receive dan unique raqamlar: ${unique.length}`);
-    return unique.slice(0, 24);  // Faqat 4 ta (birinchi sahifa uchun)
+    return unique.slice(0, 56);  // Faqat 4 ta (birinchi sahifa uchun)
   } catch (err) {
     console.error('scrapeSite failed', url, err && err.message);
     return [];
